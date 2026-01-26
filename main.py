57138f47bad9b0e6a78dac0f3451ea19e919c37c
Ilya <placeholder@mail.ru>
2026-01-26 20:30:18 +0300

    added g(x)

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 def f(x):
     return x ** 2
 
+
+def g(x):
+    return x ** 3
+
+
 name = input("What is your name?")
 
 print("hello world!")
 print(f"privet {name}!")
 
-x = 2 * f(55) + 10
+x = g(2) * f(55) + 10
